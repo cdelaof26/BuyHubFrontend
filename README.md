ce6c1c24f0a7fbcb8804e5163d4ca0bdc373d034
cdelaof26 <cdelaof1800@alumno.ipn.mx>
2025-06-10 20:23:30 -0600

    Component breakup

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ E-Commerce mobile-only frontend prototype
 
 ### Versioning
 
+#### v0.0.2 Component breakup
+
 #### v0.0.1 Initial project
--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 
-const aboutArticles = ["my-whoami", "my-aspirations", "my-skills"];
-const technologies = ["front-end", "back-end", "os", "cloud", "desktop", "scripting", "other", "tools"];
-const projects = ["libBasicUI", "IPNScheduler", "PyWST", "PointMotionLib", "finbalancpp", "clima_mx", "IP-Calc", "DefendTheClockLib", "arpspoofing_tool", "pgitHash", "escomsole", "http_server", "ESCOMPong"];
-
-let last_section = "header";
-let lang_menu_visible = false;
-
 function toggle_class(add, element, ...classes) {
     if (typeof element === "string")
         element = document.getElementById(element);
@@ -21,138 +14,115 @@ function toggle_class(add, element, ...classes) {
     });
 }
 
-function toggle_language_menu(show) {
-    lang_menu_visible = show;
-    let menu = document.getElementById("lang-menu");
-    let langButton = document.getElementById("lang-button");
-    toggle_class(!show, "chevron-up", "hidden");
-    toggle_class(show, "chevron-down", "hidden");
-    toggle_class(!show, langButton, "rounded-lg");
-    toggle_class(show, langButton, "rounded-t-lg");
-    toggle_class(!show, menu, "opacity-[0]", "-translate-y-full");
-    toggle_class(show, menu, "lg:translate-y-10", "translate-y-8");
+function set_search_bar_state(show) {
+    const search_field = document.getElementById("middle-component-bottom");
+
+    toggle_class(!show, search_field, "invisible", "opacity-0");
+    toggle_class(show, search_field, "w-1/2");
 }
 
-function toggle_language_menu_visibility() {
-    toggle_language_menu(!lang_menu_visible);
+function toggle_search_bar() {
+    set_search_bar_state(document.getElementById("middle-component-bottom").classList.contains("invisible"));
 }
 
-function is_scrolled_into_view(element_id) {
-    const element = document.getElementById(element_id);
-    if (element === null)
-        return false;
+function set_component_config({ show_search, middle_component_text, show_trash_can }) {
+    const middle = document.getElementById("middle-component-bottom");
 
-    const doc_view_top = window.scrollY;
-    const doc_view_bottom = doc_view_top + window.innerHeight;
+    toggle_class(!show_search, "search-button", "invisible");
+    toggle_class(!show_trash_can, "trash-can", "invisible");
 
-    const element_top = element.getBoundingClientRect().top + doc_view_top;
-    const element_bottom = element_top + element.offsetHeight;
-
-    return element_bottom <= doc_view_bottom && element_top >= doc_view_top;
-}
-
-function set_section_selected(section) {
-    const id = "nav-" + section;
-    last_section = section;
-
-    ["header", "about", "portfolio"].forEach((_id) => {
-        _id = "nav-" + _id;
-        const e = document.getElementById(_id);
-        toggle_class(id === _id, e, "border-indigo-600", "dark:border-indigo-500");
-        toggle_class(id !== _id, e, "border-transparent");
-    });
-}
-
-function toggle_project_set(type) {
-    const id = type + "-projects";
-
-    const esp = ["todos", "front", "escritorio", "scripting", "otros"];
-    ["all", "front-end", "desktop", "scripting", "others"].forEach((_id, i) => {
-        const initial_id = (is_spanish ? esp[i] : _id).replace("-end", "");
-
-        _id = _id + "-projects";
-        const title_element = document.getElementById(_id);
-        const parent = title_element.parentElement;
-
-        toggle_class(id !== _id, title_element, "lg:hidden");
-        toggle_class(id !== _id, parent, "bg-body-0", "dark:bg-body-1", "lg:w-16");
-        toggle_class(id === _id, parent, "lg:w-48", "text-white", "bg-gradient-to-bl", "from-indigo-500", "to-indigo-700", "dark:from-indigo-700", "dark:to-indigo-900");
-
-        if (id === _id)
-            projects.forEach(p => {
-                const element = document.getElementById(p + "-project-article");
-                if (i === 0)
-                    toggle_class(false, element, "hidden");
-                else
-                    toggle_class(
-                        initial_id === "all" || !document.getElementById(p + "-category").textContent.toLowerCase().includes(initial_id),
-                        element, "hidden"
-                    );
-            });
-    });
-}
-
-function header_scroll_into_view(evt) {
-    let id;
-    if (evt.target.id === null || !evt.target.id.includes("-header-btn"))
-        id = evt.target.parentElement.id.replace("-header-btn", "");
-    else
-        id = evt.target.id.replace("-header-btn", "");
-
-    const element = document.getElementById(id);
-    if (element === null)
+    if (show_search) {
+        middle.children[0].type = "text";
+        middle.children[0].value = "";
+        set_search_bar_state(false);
         return;
-    element.scrollIntoView();
+    }
+
+    set_search_bar_state(true);
+
+    if (middle_component_text !== null) {
+        middle.children[0].type = "button";
+        middle.children[0].value = middle_component_text;
+    }
 }
 
-document.addEventListener("scroll", () => {
-    if (is_scrolled_into_view("career")) {
-        set_section_selected("header");
+let saved_config;
 
-        aboutArticles.forEach(_id => {
-            toggle_class(true, _id + "-article", "-translate-x-full", "opacity-[0]");
-        });
+function get_current_config() {
+    const middle = document.getElementById("middle-component-bottom");
+    const show_search = !document.getElementById("search-button").classList.contains("invisible");
 
-        technologies.forEach((_id) => {
-            toggle_class(true, _id + "-article", "translate-y-full", "opacity-[0]");
-        });
+    return {
+        show_search: middle.children[0].type === "text",
+        middle_component_text: show_search ? null : middle.children[0].value,
+        show_trash_can: !document.getElementById("trash-can").classList.contains("invisible")
+    };
+}
 
-        projects.forEach((_id) => {
-            toggle_class(true, _id + "-project-article", "translate-y-full", "opacity-[0]");
-        });
+function show_menu() {
+    const element = document.getElementById("main-menu");
+    toggle_class(false, element, "-translate-x-full");
+}
+
+function close_menu(evt) {
+    let sender = null;
+    let evt_parent = evt.target.parentElement;
+    while (sender === null) {
+        sender = evt_parent.getAttribute("sender");
+        evt_parent = evt_parent.parentElement;
     }
 
-    if (is_scrolled_into_view("my-whoami-article") || is_scrolled_into_view("my-skills-article")) {
-        set_section_selected("about");
-        aboutArticles.forEach(_id => {
-            toggle_class(false, _id + "-article", "-translate-x-full", "opacity-[0]");
-        });
+    const element = document.getElementById(sender);
+    if (sender === "main-menu") {
+        toggle_class(true, element, "-translate-x-full");
+        return;
     }
 
-    if (is_scrolled_into_view("front-end-article") || is_scrolled_into_view("os-article")
-        || is_scrolled_into_view("desktop-article") || is_scrolled_into_view("other-article")) {
-        set_section_selected("about");
+    element.parentElement.removeChild(element);
+    set_component_config(saved_config);
+}
 
-        technologies.forEach((_id) => {
-            toggle_class(false, _id + "-article", "translate-y-full", "opacity-[0]");
-        });
-    }
+function show_page(component, component_id, bottom_component_options) {
+    set_search_bar_state(false);
+    const container = document.getElementById("emergent-content");
+    if (container.children.length === 1)
+        container.removeChild(container.children[0]);
 
-    if (is_scrolled_into_view("project-filter") || is_scrolled_into_view("IPNScheduler-project-article")
-        || is_scrolled_into_view("PointMotionLib-project-article") || is_scrolled_into_view("clima_mx-project-article")
-        || is_scrolled_into_view("DefendTheClockLib-project-article") || is_scrolled_into_view("pgitHash-project-article")
-        || is_scrolled_into_view("http_server-project-article")) {
+    component.id = component_id;
+    container.appendChild(component);
 
-        set_section_selected("portfolio");
-        projects.forEach((_id) => {
-            toggle_class(false, _id + "-project-article", "translate-y-full", "opacity-[0]");
-        });
-    }
-});
+    saved_config = get_current_config();
+    set_component_config(bottom_component_options);
+}
+
+function show_login() {
+    show_page(login(), "login-form", {
+        show_search: false, middle_component_text: "Iniciar sesión", show_trash_can: false
+    });
+}
+
+function load_bag() {
+    show_page(bag(), "bag-screen", {
+        show_search: false, middle_component_text: "Finalizar compra", show_trash_can: true
+    });
+}
+
+function load_article({imageUrl, priceTag, name, description}) {
+    show_page(product_page(imageUrl, priceTag, name, description), "article-screen", {
+        show_search: true, middle_component_text: null, show_trash_can: false
+    });
+}
+
+function show_create_product() {
+    show_page(create_product(), "create-article-screen", {
+        show_search: false, middle_component_text: "Agregar producto", show_trash_can: false
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.addEventListener("click", (evt) => {
-        if (!document.getElementById("lang-div").contains(evt.target))
-            toggle_language_menu(false);
-    });
+    const n = nav("store_button");
+    document.getElementById("main-nav").replaceWith(n);
+    const h = home();
+    h.id = "app-body";
+    document.getElementById("app-body").replaceWith(h);
 });
